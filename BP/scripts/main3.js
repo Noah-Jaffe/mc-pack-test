c64429efad41c411a2cd1b6fd3fc0fbb1c66175e
njaffe <noah0m0jaffe@gmail.com>
2026-04-02 12:41:13 -0400

    Update main3.js

--- BP/scripts/main3.js
+++ BP/scripts/main3.js
@@ -4,222 +4,236 @@ const startJobId = `${scriptPrefix}:start`
 const stopJobId = `${scriptPrefix}:stop`
 const debugJobId = `${scriptPrefix}:dbg`
 const colorCodePrefix = {
-  "black": "§0",
-  "dark_blue": "§1",
-  "dark_green": "§2",
-  "dark_aqua": "§3",
-  "dark_red": "§4",
-  "dark_purple": "§5",
-  "gold": "§6",
-  "debug": "§6",
-  "gray": "§7",
-  "dark_gray": "§8",
-  "blue": "§9",
-  "green": "§a",
-  "aqua": "§b",
-  "red": "§c",
-  "error": "§c",
-  "light_purple": "§d",
-  "yellow": "§e",
-  "warning": "§e",
-  "white": "§f",
-  "info": "§f",
-  "minecoin_gold": "§g",
-  "material_quartz": "§h",
-  "material_iron": "§i",
-  "material_netherite": "§j",
-  "material_redstone": "§m",
-  "material_copper": "§n",
-  "material_gold": "§p",
-  "material_emerald": "§q",
-  "material_diamond": "§s",
-  "material_lapis": "§t",
-  "material_amethyst": "§u",
-  "material_resin": "§v",
-  "reset": "§r",
-  "obfuscate": "§k",
-  "bold": "§l",
-  "italic": "§o",
-  
+	"black": "§0",
+	"dark_blue": "§1",
+	"dark_green": "§2",
+	"dark_aqua": "§3",
+	"dark_red": "§4",
+	"dark_purple": "§5",
+	"gold": "§6",
+	"debug": "§6",
+	"gray": "§7",
+	"dark_gray": "§8",
+	"blue": "§9",
+	"green": "§a",
+	"aqua": "§b",
+	"red": "§c",
+	"error": "§c",
+	"light_purple": "§d",
+	"yellow": "§e",
+	"warning": "§e",
+	"white": "§f",
+	"info": "§f",
+	"minecoin_gold": "§g",
+	"material_quartz": "§h",
+	"material_iron": "§i",
+	"material_netherite": "§j",
+	"material_redstone": "§m",
+	"material_copper": "§n",
+	"material_gold": "§p",
+	"material_emerald": "§q",
+	"material_diamond": "§s",
+	"material_lapis": "§t",
+	"material_amethyst": "§u",
+	"material_resin": "§v",
+	"reset": "§r",
+	"obfuscate": "§k",
+	"bold": "§l",
+	"italic": "§o",
+	
 }
 // Store current job + cancel state
 const SCRIPT_STATE = {
-    activeJob: null,
-    cancelRequested: false,
-    debug: false
+	activeJob: null,
+	cancelRequested: false,
+	debug: false,
+	root: null,
+	step: null,
+	
 }
 
+const chunkSize = 16;
+
 function resetJobState(scriptState) {
-	scriptState.lastCalled="resetJobState"
-    scriptState.activeJob = null;
-    scriptState.cancelRequested = false;
-    try {
-        world.sendMessage(`${colorCodePrefix.warning}${scriptPrefix} stopped successfully!`)
-    } catch {
-        console.error(e);
-    }
+	// 	scriptState.lastCalled="resetJobState"
+	scriptState.activeJob = null;
+	scriptState.cancelRequested = false;
+	try {
+		world.sendMessage(`${colorCodePrefix.warning}${scriptPrefix} stopped successfully!`)
+	} catch (e) {
+		console.error(e);
+	}
 }
 
 /**
- * Example generator job
- * This simulates chunk generation work over time
- */
+* Example generator job
+* This simulates chunk generation work over time
+*/
 function* chunkGenerator(scriptState, startingLoc=null, event=null) {
-  scriptState.lastCalled="chunkGenerator";
-
-    // let failsafe = (() => {const abortTick = system.currentTick + 1200; return () => {return system.currentTick < abortTick} })();
-    let i = 0;
-    let lastActivityTick = system.currentTick;
-    if (!startingLoc) {
-        startingLoc = new Vector3(0,128,0);
-    }
-    world.sendMessage(`${colorCodePrefix.green}@ ${startingLoc.x} ${startingLoc.z}`)
-    world.sendMessage(`${colorCodePrefix.info}dbg: ${scriptState.activeJob} ${scriptState.cancelRequested}`)
-
-    try {
-        while (i < 10) {
-        	if (scriptState.debug) { popupDisplay(event, scriptState, `tick: ${system.currentTick}\tlastTick: ${lastActivityTick}`)  }
-          //  world.sendMessage(`${colorCodePrefix.info}dbg: ${scriptState.activeJob} ${scriptState.cancelRequested} ${i} ${system.currentTick}`)
-        // for (const chunkToLoad of walkChunkTaxicab(scriptState)) {
-            // Check cancel flag every iteration
-            if (scriptState.cancelRequested) {
-                world.sendMessage(`${colorCodePrefix.warning}: abort flag recognized!`);
-                return resetJobState();
-            }
-            let currentTick = system.currentTick;
-            // Simulated work (replace with real logic)
-            if (currentTick - lastActivityTick >= 60) {
-                world.sendMessage(`${colorCodePrefix.yellow}${scriptPrefix} ${colorCodePrefix.green}#${i} ${colorCodePrefix.gold}@ ${currentTick}`);
-                i++;
-                lastActivityTick = currentTick;
-            }
-            // Yield so the game doesn't freeze
-            yield;
-        }
-    } catch (e) {
-        world.sendMessage(`${colorCodePrefix.yellow}${scriptPrefix} ${colorCodePrefix.error}Error occured during runtime!`);
-        world.sendMessage(`${colorCodePrefix.error}${e}`);
-        console.error(e);
-    }
-    world.sendMessage(`${colorCodePrefix.blue} ?????`)
-    return resetJobState();
+	//   scriptState.lastCalled="chunkGenerator";
+	
+	// let failsafe = (() => {const abortTick = system.currentTick + 1200; return () => {return system.currentTick < abortTick} })();
+	let i = 0;
+	let lastActivityTick = system.currentTick;
+	if (!startingLoc) {
+		startingLoc = scriptState.root ?? {x:0,z:0};
+	}
+	scriptState.root = {x:startingLoc.x, z: startingLoc.z};
+	world.sendMessage(`${colorCodePrefix.green}@ ${startingLoc.x} ${startingLoc.z}`)
+	world.sendMessage(`${colorCodePrefix.info}dbg: ${scriptState.activeJob} ${scriptState.cancelRequested}`)
+	try {
+		
+		if (scriptState.debug) { popupDisplay(event, scriptState, `tick: ${system.currentTick}\tlastTick: ${lastActivityTick}`)  }
+		//  world.sendMessage(`${colorCodePrefix.info}dbg: ${scriptState.activeJob} ${scriptState.cancelRequested} ${i} ${system.currentTick}`)
+		let n = parseInt(scriptState.step) || 0;
+		for (const chunkToLoad of walkChunkTaxicab(scriptState)) {
+			n++;
+			// Check cancel flag every iteration
+			if (scriptState.cancelRequested) {
+				break;
+			}
+			let currentTick = system.currentTick;
+			// Simulated work (replace with real logic)
+			if (currentTick - lastActivityTick >= 60) {
+				world.sendMessage(`${colorCodePrefix.yellow}${scriptPrefix} ${colorCodePrefix.green}#${i} ${colorCodePrefix.gold}@ ${currentTick}`);
+				i++;
+				lastActivityTick = currentTick;
+			}
+			// Yield so the game doesn't freeze
+			yield;
+		}
+		if (scriptState.cancelRequested) {
+			scriptState.step = n;
+			world.sendMessage(`${colorCodePrefix.warning}: abort flag recognized!`);
+			return resetJobState(scriptState);
+		}
+		
+	} catch (e) {
+		world.sendMessage(`${colorCodePrefix.yellow}${scriptPrefix} ${colorCodePrefix.error}Error occured during runtime!`);
+		world.sendMessage(`${colorCodePrefix.error}${e}`);
+		console.error(e);
+	}
+	world.sendMessage(`${colorCodePrefix.blue} ?????`)
+	return resetJobState(scriptState);
 }
 
 // @note: infinite generator
-function* walkChunkTaxicab(center) {
-  scriptState.lastCalled="walkChunkTaxicab";
-
-    const baseX = roundForChunkEdge(center.x);
-    const baseZ = roundForChunkEdge(center.z);
-
-    // Always yield center first
-    yield { x: baseX, z: baseZ };
-    let r = 1;
-    while (true) {
-        let x = 0;
-        let z = -r;
-
-        // 4 sides of the diamond (clockwise)
-
-        // Top-right edge
-        while (x < r && z < 0) {
-            yield {
-                x: baseX + x * 16,
-                z: baseZ + z * 16,
-            };
-            x++;
-            z++;
-        }
-
-        // Bottom-right edge
-        while (x > 0 && z < r) {
-            yield {
-                x: baseX + x * 16,
-                z: baseZ + z * 16,
-            };
-            x--;
-            z++;
-        }
-
-        // Bottom-left edge
-        while (x > -r && z > 0) {
-            yield {
-                x: baseX + x * 16,
-                z: baseZ + z * 16,
-            };
-            x--;
-            z--;
-        }
-
-        // Top-left edge
-        while (x < 0 && z > -r) {
-            yield {
-                x: baseX + x * 16,
-                z: baseZ + z * 16,
-            };
-            x++;
-            z--;
-        }
-    }
+function* walkChunkTaxicab(scriptState) {
+	//  // scriptState.lastCalled="walkChunkTaxicab";
+	const center = scriptState.root;
+	
+	const baseX = roundForChunkEdge(center.x);
+	const baseZ = roundForChunkEdge(center.z);
+	
+	// Always yield center first
+	yield { x: baseX, z: baseZ };
+	let r = (scriptState.step ?? 0 )|| 0;
+	while (!scriptState.cancelRequested) {
+		r++;
+		let x = 0;
+		let z = -r;
+		
+		// 4 sides of the diamond (clockwise)
+		
+		// Top-right edge
+		while (x < r && z < 0) {
+			yield {
+				x: baseX + x * chunkSize,
+				z: baseZ + z * chunkSize,
+			};
+			x++;
+			z++;
+		}
+		
+		// Bottom-right edge
+		while (x > 0 && z < r) {
+			yield {
+				x: baseX + x * chunkSize,
+				z: baseZ + z * chunkSize,
+			};
+			x--;
+			z++;
+		}
+		
+		// Bottom-left edge
+		while (x > -r && z > 0) {
+			yield {
+				x: baseX + x * chunkSize,
+				z: baseZ + z * chunkSize,
+			};
+			x--;
+			z--;
+		}
+		
+		// Top-left edge
+		while (x < 0 && z > -r) {
+			yield {
+				x: baseX + x * chunkSize,
+				z: baseZ + z * chunkSize,
+			};
+			x++;
+			z--;
+		}
+	}
 }
 
 
 function roundForChunkEdge(value) {
-  scriptState.lastCalled="roundForChunkEdge"
-  const chunkSize = 15;
-
-    if (value >= 0) {
-        return value - (value % 16);
-    }
-    else {
-        return value - (((value % 16) + 16) % 16);
-    }
+	//   // scriptState.lastCalled="roundForChunkEdge"
+	if (value >= 0) {
+		return value - (value % chunkSize);
+	}
+	else {
+		return value - (((value % chunkSize) + chunkSize) % chunkSize);
+	}
 }
 
 /**
- * Start the job safely
- */
+* Start the job safely
+*/
 function startJob(event, scriptState) {
-  scriptState.lastCalled="startJob";
-
-    if (scriptState.activeJob) {
-        world.sendMessage(`${colorCodePrefix.error}${scriptPrefix} is already running! to stop, run: ${colorCodePrefix.white}/scriptEvent ${stopJobId}`);
-        return;
-    }
-
-    scriptState.cancelRequested = false;
-    const startingLoc = event?.sourceEntity?.location;
-    const job = chunkGenerator(scriptState, startingLoc, event);
-    scriptState.activeJob = system.runJob(job);
-
-    world.sendMessage(`${colorCodePrefix.warning}${scriptPrefix} started id: ${scriptState.activeJob}`);
+	//   scriptState.lastCalled="startJob";
+	
+	if (scriptState.activeJob) {
+		world.sendMessage(`${colorCodePrefix.error}${scriptPrefix} is already running! to stop, run: ${colorCodePrefix.white}/scriptEvent ${stopJobId}`);
+		return;
+	}
+	
+	scriptState.cancelRequested = false;
+	const startingLoc = event?.sourceEntity?.location;
+	scriptState.root = {x:startingLoc.x, z: startingLoc.z};
+	scriptState.step = 0;
+	const job = chunkGenerator(scriptState, startingLoc, event);
+	scriptState.activeJob = system.runJob(job);
+	
+	world.sendMessage(`${colorCodePrefix.warning}${scriptPrefix} started id: ${scriptState.activeJob}`);
 }
 
 /**
- * Stop the job safely
- */
+* Stop the job safely
+*/
 function stopJob(event, scriptState) {
-  scriptState.lastCalled="stopJob";
-
-    if (scriptState.activeJob == null) {
-        world.sendMessage(`${colorCodePrefix.error}${scriptPrefix} is not running! To start, run: ${colorCodePrefix.white}/scriptEvent ${startJobId}`);
-        return;
-    } else {
-        try {
-            system.clearJob(scriptState.activeJob);
-        } catch (e){
-            console.error(e);
-        }
-    }
-
-    // Signal the generator to stop
-    scriptState.cancelRequested = true;
+	//   scriptState.lastCalled="stopJob";
+	
+	if (scriptState.activeJob == null) {
+		world.sendMessage(`${colorCodePrefix.error}${scriptPrefix} is not running! To start, run: ${colorCodePrefix.white}/scriptEvent ${startJobId}`);
+		return;
+	} else {
+		try {
+			system.clearJob(scriptState.activeJob);
+		} catch (e){
+			console.error(e);
+		}
+	}
+	
+	// Signal the generator to stop
+	scriptState.cancelRequested = true;
 }
 
 function debugJob(event, scriptState) {
-	scriptState.lastCalled="debugJob"
-  scriptState.debug = !scriptState.debug;
-  world.sendMessage(`${colorCodePrefix.info}Set debug mdoe to: ${scriptState.debug ? colorCodePrefix.green : colorCodePrefix.red}${scriptState.debug}`)
+	// 	scriptState.lastCalled="debugJob"
+	scriptState.debug = !scriptState.debug;
+	world.sendMessage(`${colorCodePrefix.info}Set debug mdoe to: ${scriptState.debug ? colorCodePrefix.green : colorCodePrefix.red}${scriptState.debug}`)
 }
 
 function popupDisplay(event, scriptState) {
@@ -243,7 +257,7 @@ function popupDisplay(event, scriptState) {
 		try {
 			let js;
 			if (typeof a == "object") {
-			js = JSON.stringify(arguments[a], null, 0)
+				js = JSON.stringify(arguments[a], null, 0)
 			} else {
 				js = arguments[a].toString()
 			}
@@ -256,25 +270,66 @@ function popupDisplay(event, scriptState) {
 }
 
 /**
- * Listen for script events
- */
-system.afterEvents.scriptEventReceive.subscribe((event) => {
-    const jobHandler = {
-        [startJobId]: startJob,
-        [stopJobId]: stopJob,
-        [debugJobId]: debugJob,
-    }
-    
-    if (event.id in jobHandler) {
-        world.sendMessage(`${colorCodePrefix.info}Attempting to start: ${colorCodePrefix.green}${event.id}`)
-        try {
-            jobHandler[event.id](event, SCRIPT_STATE);
-        } catch (e) {
-            world.sendMessage(`${colorCodePrefix.error}Error in: ${event.id}`);
-            world.sendMessage(`${colorCodePrefix.error}${e}`);
-            console.error(e);
-        }
-        world.sendMessage(`${colorCodePrefix.info}spawned job: ${colorCodePrefix.blue}${event.id}`)
-    }
-    
-});
+* Listen for script events
+*/
+function recognizeMyEvents(event) {
+	
+	if (event.id in jobHandler) {
+		world.sendMessage(`${colorCodePrefix.info}Attempting to start: ${colorCodePrefix.green}${event.id}`)
+		try {
+			jobHandler[event.id](event, SCRIPT_STATE);
+		} catch (e) {
+			world.sendMessage(`${colorCodePrefix.error}Error in: ${event.id}`);
+			world.sendMessage(`${colorCodePrefix.error}${e}`);
+			console.error(e);
+		}
+		world.sendMessage(`${colorCodePrefix.info}spawned job: ${colorCodePrefix.blue}${event.id}`)
+	}
+	
+}
+
+
+function test(){
+	// debug walker code
+	n=1000
+	radius = Math.ceil(Math.sqrt(n));
+	size = radius * 2 + 1;
+	arr = new Array(size)
+	.fill(null)
+	.map(() => new Array(size).fill(null));
+	scriptState = SCRIPT_STATE 
+	scriptState.root = {x:n/12 ,z:n/12};
+	scriptState.step = 0;
+	a=walkChunkTaxicab(scriptState)
+	offset = Math.ceil(Math.ceil(radius) / 2) +2;
+	
+	console.log(scriptState, arr)
+	for (let i=0; i<n; ++i) { 
+		b=a.next();
+		// console.log(i, b.value.x,b.value.z)
+		const gridX = Math.floor(b.value.x / chunkSize) + offset;
+		const gridZ = Math.floor(b.value.z / chunkSize) + offset;
+		
+		if (arr[gridX] && arr[gridX][gridZ] !== undefined) {
+			arr[gridX][gridZ] = i;
+		} else {
+			console.warn(i, gridX, gridZ, b.value.x, b.value.z)
+		}
+	}
+	console.log(arr.map(r=>r.join("\t")).join("\n"))
+}
+
+
+const jobHandler = {
+	[startJobId]: startJob,
+	[stopJobId]: stopJob,
+	[debugJobId]: debugJob,
+};
+
+	
+if (typeof system == "undefined") {
+	test()
+} else {
+	world.sendMessage(Object.keys(jobHandler).map(k=> `/scriptevent ${k}`).join("\n"))
+	system.afterEvents.scriptEventReceive.subscribe(recognizeMyEvents);
+}
