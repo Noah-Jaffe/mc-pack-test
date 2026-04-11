db74f0ca3ddf9d769d3ab7a13becfd006bf82b08
njaffe <noah0m0jaffe@gmail.com>
2026-04-11 07:42:08 -0400

    update BP/scripts/RepeatableEvent.js

---
+++ BP/scripts/RepeatableEvent.js
@@ -0,0 +1,38 @@
+"use strict";
+import { world } from "@minecraft/server";
+
+export /*abstract*/ class RepeatableEven {
+  /**
+   * Called when the event starts.
+   * Default: does nothing.
+   * @param {Event} event - source event that Called the /scriptEvent command
+   * @param {ScriptState} scriptState - the ScriptState of the active event
+   */
+  static onStart(event/*:Event*/, scriptState/*: ScriptState*/): void {
+    // default no-op
+    world.sendMessage(`${debugPrefix()}Called ${arguments.callee.toString().replaceAll(/(?:[\s\S\r\n]*?function[\W\s]*)(.*?)(?:[\s\W][\s\S\r\n]*)/gmi, "$1")}`);
+  }
+
+  /**
+   * Called to check if event should run
+   * Default: always returns true.
+   * @param {Event} event - source event that Called the /scriptEvent command
+   * @param {ScriptState} scriptState - the ScriptState of the active event
+   */
+  static conditionCheck(event/*:Event*/, scriptState/*: ScriptState*/): boolean {
+  	world.sendMessage(`${debugPrefix()}Called ${arguments.callee.toString().replaceAll(/(?:[\s\S\r\n]*?function[\W\s]*)(.*?)(?:[\s\W][\s\S\r\n]*)/gmi, "$1")}`);
+    return true;
+  }
+
+  /**
+   * Called when stopping the event
+   * @note: should reset non-persistant values if needed
+   * Default: does nothing.
+   * @param {Event} event - source event that Called the /scriptEvent command
+   * @param {ScriptState} scriptState - the ScriptState of the active event
+   */
+  static onStop(event/*:Event*/, scriptState/*: ScriptState*/): void {
+    // default no-op
+    world.sendMessage(`${debugPrefix()}Called ${arguments.callee.toString().replaceAll(/(?:[\s\S\r\n]*?function[\W\s]*)(.*?)(?:[\s\W][\s\S\r\n]*)/gmi, "$1")}`);
+  }
+}
