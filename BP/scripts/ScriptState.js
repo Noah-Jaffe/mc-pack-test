db65b24d45d7656f529b8e8055cc5642c88bb21a
njaffe <noah0m0jaffe@gmail.com>
2026-04-11 09:04:47 -0400

    update BP/scripts/ScriptState.js

--- BP/scripts/ScriptState.js
+++ BP/scripts/ScriptState.js
@@ -1,11 +1,17 @@
 /** ScriptState holds a persistent passable object for the command */
 export /*interface*/ class ScriptState {
-	/** @propety {number|null} [id] - id of active job */
+	/** @propety {number|null} id - id of active job */
 	id;//?: number | null;
 	/** @propety {boolean} [debug] - optional debug flag */
 	debug;//?: boolean;
+	/** @property {boolean} cancelRequested - if the state has raised a cancel flag */
+	cancelRequested;//: boolean;
+	/** @propety {number} step - the current step (increments even if conditionCheck returns false) */
+	step;//: number;
+ 	
   // custom fields as needed
   // [key: string]: unknown;
+  
   
   constructor(kwArgs) {
   	if (typeof kwArgs === "object") {
