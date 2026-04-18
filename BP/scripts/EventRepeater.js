11edb8f41dd62800449f8739542344ba1f70d248
njaffe <noah0m0jaffe@gmail.com>
2026-04-18 08:50:13 -0400

    update BP/scripts/EventRepeater.js test object proto dne

--- BP/scripts/EventRepeater.js
+++ BP/scripts/EventRepeater.js
@@ -3,7 +3,7 @@ import { system, world } from "@minecraft/server";
 import { ColorCodes } from "./ColorCodes.js";
 import { ChunkLoader } from "./ChunkLoader.js";
 import { mconsole as console } from "./debug.js";
-
+mconsole._logger = world.sendMessage;
 // @todo refactor script state layout
 function roundForChunkEdge(value) {
 	if (value >= 0) {
