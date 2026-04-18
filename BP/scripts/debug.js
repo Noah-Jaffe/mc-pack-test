a93d0f3156bff09219224d7ffb7822bb6da5058f
njaffe <noah0m0jaffe@gmail.com>
2026-04-18 09:34:35 -0400

    update BP/scripts/debug.js

--- BP/scripts/debug.js
+++ BP/scripts/debug.js
@@ -149,22 +149,5 @@ system.runTimeout(()=>{
 	world.sendMessage(`debug.js seems ok!!!`);
 	
 }, 20*5);
-system.runTimeout(()=> {
-	world.sendMessage(typeof(world.sendMessage))
-	world.sendMessage(typeof(mconsole._logger))
-	try {
-		mconsole._logger = world.sendMessage
-
-	} catch(e) {
-		world.sendMessage(`${ColorCodes.error}failed to set logger?${e}`);
-	}
-		try {
-			mconsole._logger("hello world")
-		//mconsole._logger("hello world")
-	} catch(e) {
-		world.sendMessage(`${ColorCodes.error}failed to print from logger?${e}`);
-	}
-	world.sendMessage("done test")
-}, 20*6);
 
 export { mconsole }; 
