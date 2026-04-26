7e0136bb0175e9aa3e794f4f6295937bd44ee231
njaffe <noah0m0jaffe@gmail.com>
2026-04-26 08:24:00 -0400

    update BP/scripts/main.js better debug

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -2,7 +2,6 @@
 import { system, world } from "@minecraft/server";
 import { ColorCodes } from "./ColorCodes.js";
 import { mconsole as console } from "./debug.js"; 
-
 // import custom command implementations here
 import { AutoChunkGenerator } from "./AutoChunkGeneratorEvent.js";
 
@@ -12,24 +11,31 @@ const MY_PLUGINS = [
 ];
 
 // on loading the script, call the events "register" function which is responsible for subscribing to the appropriate event
+let loaded = 0;
 for (let i = 0; i < MY_PLUGINS.length; ++i) {
 	let event = MY_PLUGINS[i];
 	try {
 		if (typeof event == "function") {
 			// MY_PLUGINS entry is a class type 
 			event = new event();
+			console.log(`[${i}/${MY_PLUGINS.length}] initialized custom plug-in instance: ${event.constructor.name}`)
 		} else {
 			// MY_PLUGINS entry is an instance?
+			console.log(`[${i}/${MY_PLUGINS.length}] custom plug-in already initalized: ${event.constructor.name}`)
 		}
 		event.register();
+		console.log(`[${i}/${MY_PLUGINS.length}] custom plug-in registered: ${event.constructor.name}`)
 		MY_PLUGINS[i] = event;
+		loaded++;
 	} catch (err) {
-		console.log(`${ColorCodes.error}Error registering or spawning event ${event.constructor.name}: ${err.name}`);
+		system.runTimeout(() => {
+		console.log(`${ColorCodes.error}Error initalizing or registering event ${event.constructor.name}: ${err.name}`);
 		console.error(err);
+		}, 20*2);
 	}
 }
 
 system.runTimeout(() => {
 	world.sendMessage("loaded my plugins!")
-	console.log(`Loaded ${MY_PLUGINS.length} plugins`);
+	console.log(`Loaded ${loaded}/${MY_PLUGINS.length} plugins`);
 }, 20*5);
