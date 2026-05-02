16ef68e266c345f0a344e78514fe161bf8303a73
njaffe <noah0m0jaffe@gmail.com>
2026-05-02 09:57:38 -0400

    update BP/scripts/Utils.js added findByKeys

--- BP/scripts/Utils.js
+++ BP/scripts/Utils.js
@@ -18,10 +18,30 @@ export function flatten(obj, sep = '.') {
 	return out;
 }
 
+
+export function findByKeys(obj, keyReg) {
+	let found = 0;
+	let ret = Object.entries(obj).reduce((acc, [k,v]) => {
+		if (k.match(keyReg)) {
+			acc[k] = v;
+			found++;
+		}
+		return acc;
+	},
+	{});
+	
+	if (found == 1) {
+		ret = ret[Object.keys(ret)[0]];
+	} else if (found == 0) {
+		ret = undefined;
+	}
+	return ret;
+}
+
 /*
 const tests = {};
 tests.flatten = ()=> {
-	const toTest = [
+	const toTest = 	[
 		{ arg: { a: { b: 1 }, c: 2 }, expected: { "a.b": 1, c: 2 } },
 		{ arg: { a: { b: [1, 2] } }, expected: { "a.b[0]": 1, "a.b[1]": 2 } },
 		{ arg: { a: null, b: { c: undefined } }, expected: { a: null, "b.c": undefined } },
@@ -35,16 +55,43 @@ tests.flatten = ()=> {
 		{ arg: {}, expected: {} },
 		{ arg: { "a.b": { "c[d]": 1 } }, expected: { "a.b.c[d]": 1 } },
 		{ arg: 42, expected: { "": 42 } },
-	];
-	toTest.forEach((arg, i, arr)=>{
-		let actual = flatten(arg.arg);
-		const a = JSON.stringify(actual);
-		const e = JSON.stringify(arg.expected);
-		if(a!=e) {
-			console.error(`TEST ${i+1}/${arr.length} FAILED: flatten failed test for ${JSON.stringify(arg.arg)}.\nExpected:${e}\nActual:\n${a}`)
-		}
-	});
+		];
+		toTest.forEach((arg, i, arr)=>{
+			let actual = flatten(arg.arg);
+			const a = JSON.stringify(actual);
+			const e = JSON.stringify(arg.expected);
+			if(a!=e) {
+				console.error(`TEST ${i+1}/${arr.length} FAILED: flatten failed test for ${JSON.stringify(arg.arg)}.\nExpected:${e}\nActual:\n${a}`)
+			}
+		});
+};
+tests.findByKeys = () => {
+const toTest = [
+  { arg: [{ a: 1, b: 2 }, /a/], expected: 1 },
+  { arg: [{ a: 1, b: 2 }, /b/], expected: 2 },
+  { arg: [{ a: 1, b: 2 }, /c/], expected: undefined },
+  { arg: [{ foo: 'x', bar: 'y' }, /foo/], expected: 'x' },
+  { arg: [{ foo: 'x', bar: 'y' }, /ba/], expected: 'y' },
+  { arg: [{ foo: 'x', bar: 'y' }, /z/], expected: undefined },
+  { arg: [{ one: 1, two: 2, three: 3 }, /t/], expected: { two: 2, three: 3 } },
+  { arg: [{ one: 1, two: 2, three: 3 }, /^one$/], expected: 1 },
+  { arg: [{ 'a-b': 10, 'a_b': 20 }, /a-/], expected: 10 },
+  { arg: [{ 'a-b': 10, 'a_b': 20 }, /a_/], expected: 20 },
+  { arg: [{ nested: { x: 1 }, other: { y: 2 } }, /nested/], expected: { x: 1 } },
+  { arg: [{}, /.+/], expected: undefined },
+  { arg: [{ present: null, missing: 5 }, /present/], expected: null },
+  { arg: [{ dup1: 'v1', dup2: 'v2' }, /dup/], expected: { dup1: 'v1', dup2: 'v2' } },
+  { arg: [{ num0: 0, num1: 1 }, /num0/], expected: 0 }
+];
+		toTest.forEach((arg, i, arr)=>{
+			const actual = findByKeys(...arg.arg);
+			const a = JSON.stringify(actual);
+			const e = JSON.stringify(arg.expected);
+			if(a!=e) {
+				console.error(`TEST ${i+1}/${arr.length} FAILED: findByKeys failed test for ${JSON.stringify(arg.arg)}.\nExpected:${e}\nActual:\n${a}`)
+			}
+		});
 };
 
 (()=>Object.entries(tests).forEach(([k,v]) => v()))();
-*/
+//*/
